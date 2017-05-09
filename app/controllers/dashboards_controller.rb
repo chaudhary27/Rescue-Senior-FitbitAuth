1769ea635d2d1a4833fa28ce0dd907e2ea80139a
faisal farooq <chaudhary.fsl@gmail.com>
2017-05-09 13:05:16 -0400

    auth updates

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -6,48 +6,47 @@ class DashboardsController < ApplicationController
   
   def index
     @user_name = current_user.fitbit_client.profile[:user][:display_name]
-    @devices = current_user.fitbit_client.devices
-    @devices.each do |device|
-      @device_version = device[:device_version]
-      @device_battery = device[:battery]
-    end
+    # @devices = current_user.fitbit_client.devices
+    # @devices.each do |device|
+    #   @device_version = device[:device_version]
+    #   @device_battery = device[:battery]
+    # end
     @daily_summary = current_user.fitbit_client.daily_activity_summary[:summary][:heart_rate_zones]
     
     # @resting_heart_rate = current_user.fitbit_client.daily_activity_summary[:summary][:resting_heart_rate]
-    @inactive_minutes = current_user.fitbit_client.daily_activity_summary[:summary][:sedentary_minutes]
+    # @inactive_minutes = current_user.fitbit_client.daily_activity_summary[:summary][:sedentary_minutes]
     @step_count = current_user.fitbit_client.daily_activity_summary[:summary][:steps]
-    @very_active_minutes = current_user.fitbit_client.daily_activity_summary[:summary][:very_active_minutes]
+    # @very_active_minutes = current_user.fitbit_client.daily_activity_summary[:summary][:very_active_minutes]
     
+    # Set timestamp in firebase
     @time_stamp = Time.now.strftime("%m/%d/%Y %l:%M:%S %p")
     time_stamp_ = Bigbertha::Ref.new( 'https://rescuesenior-d31bf.firebaseio.com/time_stamp')
     time_stamp_.set(@time_stamp)
     
+    # Send heart rate data to firebase
     heart_rate_zones = Bigbertha::Ref.new( 'https://rescuesenior-d31bf.firebaseio.com/heart_rate_zones' )
     heart_rate_zones.set(@daily_summary)
     
-    user_device = Bigbertha::Ref.new( 'https://rescuesenior-d31bf.firebaseio.com/device' )
-    user_device.set(@device_version)
+    # user_device = Bigbertha::Ref.new( 'https://rescuesenior-d31bf.firebaseio.com/device' )
+    # user_device.set(@device_version)
     
-    device_battery_ = Bigbertha::Ref.new( 'https://rescuesenior-d31bf.firebaseio.com/device_battery' )
-    device_battery_.set(@device_battery)
+    # device_battery_ = Bigbertha::Ref.new( 'https://rescuesenior-d31bf.firebaseio.com/device_battery' )
+    # device_battery_.set(@device_battery)
     
     # resting_hear_rate_ = Bigbertha::Ref.new( 'https://rescuesenior-d31bf.firebaseio.com/resting_heart_rate' )
     # resting_hear_rate_.set(@resting_heart_rate)
     
+    # Send step count data to Firebase
     steps_count_ = Bigbertha::Ref.new( 'https://rescuesenior-d31bf.firebaseio.com/steps_count' )
     steps_count_.set(@step_count)
     
-    inactive_minutes_ = Bigbertha::Ref.new( 'https://rescuesenior-d31bf.firebaseio.com/inactive_minutes' )
-    inactive_minutes_.set(@inactive_minutes)
+    # inactive_minutes_ = Bigbertha::Ref.new( 'https://rescuesenior-d31bf.firebaseio.com/inactive_minutes' )
+    # inactive_minutes_.set(@inactive_minutes)
     # sleep 3
-    redirect_to "http://localhost:3000/users/1"
     
+    redirect_to "http://localhost:3000/users/1"
   end
   
-  def refresh_page
-    # sleep 5
-    # redirect_to "http://localhost:3000/users/1"
-  end
   
   private
   
